5a0cb84deba19a12e8ebcccdc82bd45939e66b7d
Maulana Ja <emailkamu@gmail.com>
2026-06-27 18:55:47 +0700

    feat: create dashboard summary

--- frontend/src/pages/DasboardPage.tsx
+++ frontend/src/pages/DasboardPage.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import { Box, Grid, Card, CardContent, Typography } from "@mui/material";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { name: "Sun", total: 4000 },
+  { name: "Mon", total: 3000 },
+  { name: "Tue", total: 2000 },
+  { name: "Wed", total: 2780 },
+  { name: "Thu", total: 1890 },
+  { name: "Fri", total: 2390 },
+];
 
 const DasboardPage = () => {
+  const SummaryCard = ({
+    title,
+    value,
+    color,
+  }: {
+    title: string;
+    value: number;
+    color: string;
+  }) => (
+    <Grid size={{ xs: 12, sm: 4 }}>
+      <Card
+        sx={{ borderLeft: `5px solid ${color}`, boxShadow: 3, borderRadius: 2 }}
+      >
+        <CardContent>
+          <Typography variant="h6">{title}</Typography>
+          <Typography variant="h4">{value.toLocaleString()}</Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
   return (
-    <div>DasboardPage</div>
-  )
-}
+    <Box sx={{ p: 2 }}>
+      <Typography variant="h4" sx={{ mb: 2 }}>
+        Dashboard Overview
+      </Typography>
+      <Grid container spacing={2}>
+        <SummaryCard title="pendapatan" value={50000} color="#4caf50" />
+        <SummaryCard title="pesanan" value={1200} color="#2196f3" />
+        <SummaryCard title="pesanan selesai" value={800} color="#ff9800" />
+      </Grid>
+      <Grid sx={{ mt: 2 }}>
+        <Typography variant="h5" sx={{ mb: 2 }}>
+          Grafik
+        </Typography>
+        <ResponsiveContainer width="100%" height={300}>
+          <BarChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="total" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      </Grid>
+    </Box>
+  );
+};
 
-export default DasboardPage
+export default DasboardPage;
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import OrdersPage from "../pages/OrdersPage.tsx";
 import CustomersPage from "../pages/CustomersPage.tsx";
+import DasboardPage from "../pages/DasboardPage.tsx";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<h1>Dashboard</h1>} />
+      <Route path="/" element={<DasboardPage />} />
       <Route path="/orders" element={<OrdersPage />} />
       <Route path="/customers" element={<CustomersPage />} />
       <Route path="/reports" element={<h1>Reports</h1>} />
